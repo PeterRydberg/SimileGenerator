2e84e9145c9b1bb0e5984dc8264938a82d5763e3
Peter Rydberg <peter.rydberg@hotmail.no>
2018-03-04 22:05:32 +0100

    Generates random word in given category

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 import requests
 import json
+import random
 
-# TODO: replace with your own app_id and app_key
 app_id = 'f4959b56'
 app_key = 'b21f533facf3f53a29016ff1b78b702f'
 
-language = 'en'
-word_id = 'test'
+#Parses a new URL for the given input
+def parseURL(lang, lexCat, wordLimit):
 
-url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
+    maxWords = {
+        'adjective': 47654,
+        'noun': 0,
+        'verb': 0
+    }
 
-r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+    #Generates a random selection for the word category
+    offset = str(random.randint(0, maxWords.get(str(lexCat), "")))
+    url = 'https://od-api.oxforddictionaries.com:443/api/v1/wordlist/' + lang + '/lexicalCategory=' + lexCat.lower() + '?offset=' + offset + '&limit=' + str(wordLimit)
+    return url
 
-print("code {}\n".format(r.status_code))
-print("text \n" + r.text)
-print("json \n" + json.dumps(r.json()))
+
+#Gets the JSON response from given URL
+r = requests.get(parseURL('en', 'adjective', '1'), headers = {'app_id': app_id, 'app_key': app_key})
+
+adjective = r.json()['results'][0].get('word', '')
+print(adjective)
